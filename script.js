999819c47c13408f4dcf9f7a9b1dbe72618dbf6e
DavidGit999 <itprog999@gmail.com>
2021-11-19 18:57:50 +0200

    Add default active slide

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    addDarkenClasses()
+  slides[activeSlide].classList.add('active')
 
-    slide.classList.add('active');
-    slide.classList.remove('darken');
-  })
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      addDarkenClasses()
+
+      slide.classList.add('active');
+      slide.classList.remove('darken');
+    })
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    })
+  }
+
+  function addDarkenClasses() {
+    slides.forEach((slide) => {
+      slide.classList.add('darken');
+    })
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  })
-}
-
-function addDarkenClasses() {
-  slides.forEach((slide) => {
-    slide.classList.add('darken');
-  })
-}
+slidesPlugin()
