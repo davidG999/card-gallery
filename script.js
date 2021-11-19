d5143f2f55a579a3f66c44aabf82581bb10848d7
DavidGit999 <itprog999@gmail.com>
2021-11-19 15:15:15 +0200

    Add brightness and hover features

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const slides = document.querySelectorAll('.slide')
 for (const slide of slides) {
   slide.addEventListener('click', () => {
     clearActiveClasses()
+    addDarkenClasses()
 
     slide.classList.add('active');
+    slide.classList.remove('darken');
   })
 }
 
 function clearActiveClasses() {
   slides.forEach((slide) => {
     slide.classList.remove('active');
+  })
+}
+
+function addDarkenClasses() {
+  slides.forEach((slide) => {
+    slide.classList.add('darken');
   })
 }
