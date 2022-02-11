6974be2a062dd286cfe322803cd0fd8fc2472c13
DavidGit999 <itprog999@gmail.com>
2022-02-11 22:12:00 +0200

    Make small redesign

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function slidesPlugin(activeSlide = 0) {
   const slides = document.querySelectorAll('.slide')
-
-  slides[activeSlide].classList.add('active')
+  addDarkenClasses()
+  // slides[activeSlide].classList.add('active')
 
   for (const slide of slides) {
     slide.addEventListener('click', () => {
